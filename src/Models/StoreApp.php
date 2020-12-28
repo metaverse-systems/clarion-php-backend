dea491f4eb339fcf740ce9183d1d8bb7ac2b4eb8
Tim Schwartz <tim@metaverse.systems>
2020-12-28 09:12:32 -0600

    Started implementing StoreApp API

---
+++ src/Models/StoreApp.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MetaverseSystems\ClarionPHPBackend\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StoreApp extends Model
+{
+    use HasFactory;
+}
